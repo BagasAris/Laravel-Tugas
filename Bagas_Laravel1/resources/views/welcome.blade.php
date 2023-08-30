c55518cf2b2af29de43eefb2542a1e9a07588815
Bagas Aris Prasetyo <arisbagas278@gmail.com>
2023-08-30 19:00:16 +0700

    Saya menambahkan Laravel untuk tugas

---
+++ Bagas_Laravel1/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Welcome</title>
+
+</head>
+<body>
+    <h1>SELAMAT DATANG!</h1>
+    <h4>Terima kasih telah bergabung di website kami, Media Belajar kita bersama!</h4>
+</body>
+</html>
